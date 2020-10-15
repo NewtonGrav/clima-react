75f3dae49a5f984ad5d4d5e987646cbe75268c40
Gonzalo <gonzalom97@outlook.com>
2020-10-15 11:31:56 -0300

    feat: State para la busqueda a realizar

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+	const [busqueda, setBusqueda] = useState({
+		ciudad: '',
+		pais: '',
+	});
+	const { ciudad, pais } = busqueda;
+
+	const handlerChange = (e) => {
+		setBusqueda({
+			...busqueda,
+			[e.target.name]: e.target.value,
+		});
+	};
+
 	return (
 		<form>
 			<div className='input-field s12'>
-				<input type='text' name='ciudad' id='ciudad' />
+				<input
+					type='text'
+					name='ciudad'
+					id='ciudad'
+					value={ciudad}
+					onChange={handlerChange}
+				/>
 				<label htmlFor='ciudad'>Ciudad</label>
 			</div>
 
 			<div className='input-field s12'>
-				<select name='pais' id='pais' value=''>
+				<select name='pais' id='pais' value={pais} onChange={handlerChange}>
 					<option value='' disabled>
 						-- Seleccionar un país --
 					</option>
