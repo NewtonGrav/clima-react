148c0041ed6d092b44a331d76b8e107129d0f94b
Gonzalo <gonzalom97@outlook.com>
2020-10-15 14:49:29 -0300

    feat: Consulta hacia api y guardado de resultado

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,26 @@ function App() {
 	const { ciudad, pais } = busqueda;
 
 	const [hayConsulta, setHayConsulta] = useState(false);
+	const [resultado, setResultado] = useState({});
 
 	useEffect(() => {
-		console.log(ciudad);
-	}, [hayConsulta])
+		const consultarApi = async () => {
+			if (hayConsulta) {
+				const appId = 'b32cc12a700aa61694e20c350de3703f';
+				const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+				const promise = await fetch(url);
+				const response = await promise.json();
+				setResultado(response);
+			}
+		};
+
+		consultarApi();
+
+		// TODO resetear hayConsulta
+		
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [hayConsulta]);
 
 	return (
 		<Fragment>
@@ -23,7 +39,11 @@ function App() {
 				<div className='contenedor'>
 					<div className='row'>
 						<div className='col m6 s12'>
-							<Formulario busqueda={busqueda} setBusqueda={setBusqueda} setHayConsulta={setHayConsulta} />
+							<Formulario
+								busqueda={busqueda}
+								setBusqueda={setBusqueda}
+								setHayConsulta={setHayConsulta}
+							/>
 						</div>
 						<div className='col m6 s12'>Resultado</div>
 					</div>
