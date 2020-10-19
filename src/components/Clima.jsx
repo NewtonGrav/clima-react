f7f7067803788b04b4ddaa258339e787f139f2eb
Gonzalo <gonzalom97@outlook.com>
2020-10-19 15:06:49 -0300

    refact: Logo celsius

--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -13,27 +13,35 @@ const Clima = ({ resultado }) => {
 		return parseFloat(kelvin + ABSOLUTE_ZERO).toFixed(1);
 	};
 
+
+	const celsiusLogo = () => (
+		<span className='ml-2 is-size-6 is-align-self-flex-start'>°C</span>
+	);
+
+
 	return (
 		<div className='card'>
 			<div className='card-content'>
 				<div className='content has-black-text has-text-centered'>
 					<h1 className='title'>{name}</h1>
 
-					<h2 className='has-text-link is-size-1 has-text-weight-bold		'>
-						{kelvinToCelsius(main?.temp)} <span>°C</span>
+						<h2 className='is-flex has-text-link is-size-1 has-text-weight-bold	my-0'>
+							<span>{kelvinToCelsius(main?.temp)}</span>
+							{celsiusLogo()}
 					</h2>
+					</div>
 
 					<div className='is-size-3'>
 						<p className='is-flex is-justify-content-center'>
 							<i className='material-icons'>arrow_drop_up</i>
 							{kelvinToCelsius(main?.temp_max)}{' '}
-							<span className='ml-2 is-size-6 is-align-self-flex-start'>°C</span>
+							{celsiusLogo()}
 						</p>
 
 						<p className='is-flex is-justify-content-center'>
 							<i className='material-icons'>arrow_drop_down</i>
 							{kelvinToCelsius(main?.temp_min)}{' '}
-							<span className='ml-2 is-size-6 is-align-self-flex-start'>°C</span>
+							{celsiusLogo()}
 						</p>
 					</div>
 				</div>
