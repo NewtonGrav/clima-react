f85195f85598d0a66570d20edcd4bfa5b410a711
Gonzalo <gonzalom97@outlook.com>
2020-10-19 15:41:27 -0300

    refact: Estructura columna de los resultados

--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -13,11 +13,9 @@ const Clima = ({ resultado }) => {
 		return parseFloat(kelvin + ABSOLUTE_ZERO).toFixed(1);
 	};
 
-
 	const celsiusLogo = () => (
 		<span className='ml-2 is-size-6 is-align-self-flex-start'>°C</span>
 	);
-
 
 	return (
 		<div className='card'>
@@ -25,29 +23,41 @@ const Clima = ({ resultado }) => {
 				<div className='content has-black-text has-text-centered'>
 					<h1 className='title'>{name}</h1>
 
-					<div className='is-flex is-align-items-center	'>
-						<img
-							src={`http://openweathermap.org/img/wn/${resultado.weather[0].icon}@2x.png`}
-							alt='Icon weather'
-							className=''
-						/>
-						<h2 className='is-flex has-text-link is-size-1 has-text-weight-bold	my-0'>
-							<span>{kelvinToCelsius(main?.temp)}</span>
-							{celsiusLogo()}
-					</h2>
-					</div>
+					<div className='columns'>
+						<div className='column'>
+							<div className='is-flex is-justify-content-center	is-align-items-center	'>
+								<img
+									src={`http://openweathermap.org/img/wn/${resultado.weather[0].icon}@2x.png`}
+									alt='Icon weather'
+									className=''
+								/>
+
+								<h2 className='is-flex has-text-link is-size-1 has-text-weight-bold	my-0'>
+									<span>{kelvinToCelsius(main?.temp)}</span>
+									{celsiusLogo()}
+								</h2>
+							</div>
 
 							<p className='is-flex is-justify-content-center is-size-5 has-text-weight-bold'>
 								<span className='mr-2'>ST: </span>
 								{kelvinToCelsius(main?.feels_like)}
-							{celsiusLogo()}
-						</p>
+								{celsiusLogo()}
+							</p>
+						</div>
 
-						<p className='is-flex is-justify-content-center'>
-							<i className='material-icons'>arrow_drop_down</i>
-							{kelvinToCelsius(main?.temp_min)}{' '}
-							{celsiusLogo()}
-						</p>
+						<div className='column'>
+							<div className='is-flex is-justify-content-space-between is-size-5'>
+								<p className='is-flex my-0'>
+									<i className='material-icons is-align-self-center'>arrow_drop_down</i>
+									{kelvinToCelsius(main?.temp_min)} {celsiusLogo()}
+								</p>
+
+								<p className='is-flex'>
+									<i className='material-icons is-align-self-center'>arrow_drop_up</i>
+									{kelvinToCelsius(main?.temp_max)} {celsiusLogo()}
+								</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
